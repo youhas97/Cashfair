deaca6550fb989ebffe74264bae69e49385ccede
Yousef Hashem <youha847@student.liu.se>
2021-04-22 15:21:31 +0200

    Refactor class components into function components

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "../../styling/dashboard/Dashboard.css"
 
 import OverviewCard from "./OverviewCard"
 import CollapseableComponent from "../CollapsibleComponent"
 
-class Dashboard extends Component {
+function Dashboard() {
+  const groupCards = {}
+
+  return (
+    <div className="dashboard">
+      <CollapseableComponent
+      item={{children: [<OverviewCard key={1} item={{value: -19, className: "self-balance-card"}} />],
+             title: "Self",
+            className: "self-balance-card-container"}}
+      />
+      <CollapseableComponent
+        item={{
+          children: [
+            <OverviewCard key={1} item={{value: -100, className: "groups-card"}}/>,
+            <OverviewCard key={2} item={{value: 150, className:"groups-card"}}/>,
+            <OverviewCard key={3} item={{value: -69, className:"groups-card"}}/>
+          ],
+          title: "Groups",
+          className: "groups-card-container"
+        }}
+      />
+    </div>
+  )
+}
+
+/*class Dashboard extends Component {
   constructor() {
       super()
       this.handleChange = this.handleChange.bind(this)
@@ -15,29 +40,8 @@ class Dashboard extends Component {
   }
 
   render() {
-    const groupCards = {}
 
-    return (
-      <div className="dashboard" onClick={this.handleChange}>
-        <CollapseableComponent
-        item={{children: [<OverviewCard key={1} item={{value: -19, className: "self-balance-card"}} />],
-               title: "Self",
-              className: "self-balance-card-container"}}
-        />
-        <CollapseableComponent
-          item={{
-            children: [
-              <OverviewCard key={1} item={{value: -100, className: "groups-card"}}/>,
-              <OverviewCard key={2} item={{value: 150, className:"groups-card"}}/>,
-              <OverviewCard key={3} item={{value: -69, className:"groups-card"}}/>
-            ],
-            title: "Groups",
-            className: "groups-card-container"
-          }}
-        />
-      </div>
-    )
   }
-}
+}*/
 
 export default Dashboard
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,44 +1,30 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
 
 import "../../styling/navbar/Navbar.css"
 import NavButton from "./NavButton"
 
 import buttonData from "../../data/buttonData"
 
-class Navbar extends Component {
-  constructor() {
-      super()
-      this.state = {
-          buttons: localStorage.btnData ? JSON.parse(localStorage.btnData) : buttonData,
-          activeButtonId: 1
-      }
-      this.handleChange = this.handleChange.bind(this)
+function Navbar(props) {
+  const [buttons, setButtons] = useState(localStorage.btnData ? JSON.parse(localStorage.btnData) : buttonData)
+
+  function handleChange(id) {
+    setButtons(prevButtons => prevButtons.map(btn => {
+      btn.active = (btn.id === id) ? true : false
+      return btn
+    }))
+
+    localStorage.btnData = JSON.stringify(buttons)
   }
 
-  handleChange(id) {
-    this.setState(prevState => {
-      const updatedButtons = prevState.buttons.map(btn => {
-        btn.active = (btn.id === id) ? true : false;
-        return btn;
-      })
-      localStorage.btnData = JSON.stringify(updatedButtons)
-      return {
-        buttons: updatedButtons,
-        activeButtonId: id
-      }
-    })
-  }
+  const navButtons = buttons.map(btn =>
+    <NavButton key={btn.id} item={btn} handleChange={handleChange}/>)
 
-  render() {
-    const navButtons = this.state.buttons.map(btn =>
-      <NavButton key={btn.id} item={btn} handleChange={this.handleChange}/>)
-
-    return (
-      <nav className="Navbar">
-        {navButtons}
-      </nav>
-    )
-  }
+  return (
+  <nav className="Navbar">
+    {navButtons}
+  </nav>
+  )
 }
 
 export default Navbar
