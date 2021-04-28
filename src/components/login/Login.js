8a34e9aa545231f0d1094f69364ca3c503d3918a
Yousef Hashem <youha847@student.liu.se>
2021-04-28 15:35:02 +0200

    Fix display bug

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -9,7 +9,7 @@ function Login() {
   const [showLoginScreen, setShowLoginScreen] = useState(true)
 
   return (
-    <main>
+    <main className="login-main">
       <div className="content-div">
         {showLoginScreen ? <LoginForm /> : <RegisterForm />}
       </div>
