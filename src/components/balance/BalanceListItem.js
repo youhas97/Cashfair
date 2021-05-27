9334743d551f34bd23cbef0cc784e2ed9d21569d
Yousef Hashem <youha847@student.liu.se>
2021-05-27 16:31:15 +0200

    Prepare for SWISH API

--- src/components/balance/BalanceListItem.js
+++ src/components/balance/BalanceListItem.js
@@ -4,9 +4,13 @@ import {ListItem, ListItemText} from "@material-ui/core"
 import BalanceText from "./BalanceText"
 
 function BalanceListItem(props) {
+  const handleClick = (e) => {
+    e.preventDefault(e)
+    alert("Make Swish API call")
+  }
 
   return (
-    <ListItem divider={true} button={props.clickable}>
+    <ListItem divider={true} button={props.clickable} onClick={handleClick}>
       <ListItemText primary={props.name} />
       {props.value ? <BalanceText value={props.value}></BalanceText> : undefined}
     </ListItem>
