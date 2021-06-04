4e6f4c2b85da77dd97aefc2a5d94101c54f8a5cb
Yousef Hashem <youha847@student.liu.se>
2021-06-04 16:38:32 +0200

    Remove unused code

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -4,14 +4,3 @@ from flask_jwt_extended import JWTManager
 from flask import request
 
 jwt = JWTManager()
-
-# Wrapper than only lets through authenticated users
-def authenticated_only(f):
-  @functools.wraps(f)
-  def wrapped(*args, **kwargs):
-    print(request.headers)
-    # if not current_user.is_authenticated:
-    #   disconnect()
-    # else:
-    return f(*args, **kwargs)
-  return wrapped
